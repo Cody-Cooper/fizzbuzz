027f5c84d55990b094b5e155aaf2027d8a3a8857
Cody Cooper <contact@itscodycooper.com>
2015-11-24 00:28:00 -0800

    Changed from until loop to loop method.

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -3,18 +3,38 @@ current_number = Integer(gets.chomp)
 puts "What number would you like to end at?"
 end_number = Integer(gets.chomp)
 
-until current_number > end_number
+current_number -= 1
+
+
+loop do
+  current_number += 1
+
   if current_number % 3 == 0 && current_number % 5 == 0
     puts "Fizzbuzz!"
-    current_number += 1
   elsif current_number % 3 == 0
     puts "Fizz"
-    current_number += 1
   elsif current_number % 5 == 0
     puts "Buzz"
-    current_number += 1
   else
     puts current_number
-    current_number += 1
   end
+
+  break if current_number > end_number
 end
+
+
+# until current_number > end_number
+  # if current_number % 3 == 0 && current_number % 5 == 0
+  #   puts "Fizzbuzz!"
+  #   current_number += 1
+  # elsif current_number % 3 == 0
+  #   puts "Fizz"
+  #   current_number += 1
+  # elsif current_number % 5 == 0
+  #   puts "Buzz"
+  #   current_number += 1
+  # else
+  #   puts current_number
+  #   current_number += 1
+  # end
+# end
