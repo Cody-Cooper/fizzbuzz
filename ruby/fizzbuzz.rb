9ee0ff4a76f356134a3250f1e588e17d8d2522f8
Cody-Cooper <contact@itscodycooper.com>
2015-12-05 18:28:27 -0800

    Added HTML. Deleted some comments.

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -21,20 +21,3 @@ loop do
 
   break if current_number > end_number
 end
-
-
-# until current_number > end_number
-  # if current_number % 3 == 0 && current_number % 5 == 0
-  #   puts "Fizzbuzz!"
-  #   current_number += 1
-  # elsif current_number % 3 == 0
-  #   puts "Fizz"
-  #   current_number += 1
-  # elsif current_number % 5 == 0
-  #   puts "Buzz"
-  #   current_number += 1
-  # else
-  #   puts current_number
-  #   current_number += 1
-  # end
-# end
